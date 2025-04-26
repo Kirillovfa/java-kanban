223a6cb68a076fbe73b3194e23f058717ebb3d9d
kirandr <kirillovfa@yandex.ru>
2025-04-26 22:45:03 +0300

    feature: Доработал оформление

--- src/test/HistoryManagerTest.java
+++ src/test/HistoryManagerTest.java
@@ -8,17 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import manager.*;
 import task.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryManagerTest {
 
-    private TaskManager taskManager;
     private HistoryManager historyManager;
 
     @BeforeEach
     void setUp() {
-        taskManager = Managers.getDefault();
         historyManager = Managers.getDefaultHistory();
     }
 
